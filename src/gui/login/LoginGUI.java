7c9859ae758f9ccba2dd5d33e4b2ce00b20f3359
VascoUP <vasco_mag_pereira@hotmail.com>
2017-05-03 12:02:23 +0100

    Added button handlers

--- src/gui/login/LoginGUI.java
+++ src/gui/login/LoginGUI.java
@@ -1,15 +1,27 @@
 package gui.login;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LoginGUI extends Application {
+public class LoginGUI extends Application implements Initializable {
+    @FXML
+    private Button loginButton; // Value injected by FXMLLoader
+    @FXML
+    private Button registerButton; // Value injected by FXMLLoader
 
     public static void main(String[] args) {
         launch(args);
@@ -21,8 +33,13 @@ public class LoginGUI extends Application {
         primaryStage.setTitle("LittleChat");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
+    }
 
-        StackPane page = FXMLLoader.load(LoginGUI.class.getResource("login.fxml"));
-        Scene scene = new Scene(page);
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        loginButton.addEventHandler(MouseEvent.MOUSE_CLICKED,
+                e -> System.out.println("Login dude"));
+        registerButton.addEventHandler(MouseEvent.MOUSE_CLICKED,
+                e -> System.out.println("Register dude"));
     }
 }
