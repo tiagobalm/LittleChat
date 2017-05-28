148f8600099098ac07ebdbc6a7928e5a97cb1447
tiagobalm <tiagoalmeida.95@hotmail.com>
2017-05-28 19:23:57 +0100

    Fix LittleChat script

--- LittleChat.sh
+++ LittleChat.sh
@@ -5,4 +5,6 @@ mkdir out
 javac -d ./out -cp ".:./controlsfx-8.40.12.jar:" @sources.txt
 cp -a ./src/gui/assets ./out/gui/
 cp -a ./src/keys ./out/
+cp -a ./src/gui/login/*.fxml ./out/gui/login/
+cp -a ./src/gui/mainPage/*.fxml ./out/gui/mainPage/
 rm sources.txt
